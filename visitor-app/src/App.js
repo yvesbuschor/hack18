cecb77456b56a1831b2c15f4ee2d6d76769d9e6d
Yves Buschor <yves.buschor@gmail.com>
2018-09-15 23:15:07 +0200

    optimised icons

--- visitor-app/src/App.js
+++ visitor-app/src/App.js
@@ -8,18 +8,37 @@ import Playground from './Scenes/Playground';
 import List from './Scenes/List';
 import Detail from './Scenes/Detail';
 
+const AppContext = React.createContext({});
+
+export const AppProvider = AppContext.Provider;
+export const AppConsumer = AppContext.Consumer;
+
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL
 });
 
-const App = () => (
-  <Router>
-    <ApolloProvider client={client}>
-      <Route exact path={routes.root} component={List}/>
-      <Route path={routes.playground} component={Playground}/>
-      <Route path={routes.detail} component={Detail}/>
-    </ApolloProvider>
-  </Router>
-);
+class App extends React.Component {
+  state = {
+    filterFavOnly: false,
+  };
+
+  updateValue = (key, val) => {
+    this.setState({[key]: val});
+ }
+
+  render() {
+    return (
+      <Router>
+        <ApolloProvider client={client}>
+          <AppProvider value={this.state} >
+            <Route exact path={routes.root} component={List}/>
+            <Route path={routes.playground} component={Playground}/>
+            <Route path={routes.detail} component={Detail}/>
+          </AppProvider>
+        </ApolloProvider>
+      </Router>
+    );
+  }
+};
 
 export default App;
