a78cf9f40eb9cde277201ee9420a4453bbfdf33e
Yves Buschor <yves.buschor@gmail.com>
2018-09-16 00:22:26 +0200

    add WaitingTimeIcon Component
    Now with icons scaled to 24x24. Freshly exported, optimised and directly inserted into the component

--- visitor-app/src/Scenes/Playground/index.jsx
+++ visitor-app/src/Scenes/Playground/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import AppBar from 'Components/AppBar';
+import WaitingTimeIcon from 'Components/WaitingTimeIcon';
 
 const Playground = () => (
   <React.Fragment>
@@ -9,6 +10,10 @@ const Playground = () => (
     <Card>
       <CardContent>
         <h1>PLAYGROUND</h1>
+        <WaitingTimeIcon count={0} />
+        <WaitingTimeIcon count={1} />
+        <WaitingTimeIcon count={2} />
+        <WaitingTimeIcon count={3} />
       </CardContent>
     </Card>
   </React.Fragment>
