6efa8ebf020bbb3fa7801bfe36d48f5e761a0a90
Livio Bieri <livio@livio.li>
2018-09-16 02:42:19 +0200

    feat(qr): camera

--- visitor-app/src/Scenes/Playground/index.jsx
+++ visitor-app/src/Scenes/Playground/index.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import AppBar from 'Components/AppBar';
 import WaitingTimeIcon from 'Components/WaitingTimeIcon';
+import QrReader from 'react-qr-reader'
+
+class Test extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 100,
+    }
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan(data){
+    if (data){
+      window.location.href = data
+    }
+  }
+  handleError(err){
+    console.error(err)
+  }
+  render(){
+    return(
+      <div>
+        <QrReader
+          delay={this.state.delay}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '100%' }}
+          />
+      </div>
+    )
+  }
+  }
 
 const Playground = () => (
   <React.Fragment>
     <AppBar />
     <Card>
       <CardContent>
-        <h1>PLAYGROUND</h1>
-        <WaitingTimeIcon time={3} />
-        <WaitingTimeIcon time={10} />
-        <WaitingTimeIcon time={20} />
-        <WaitingTimeIcon time={45} />
+        <Test />
       </CardContent>
     </Card>
   </React.Fragment>
